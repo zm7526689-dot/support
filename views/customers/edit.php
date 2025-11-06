684770e9e9d8a0b08d956700e5630df56faf40c2
zm7526689-dot <zm7526689@gmail.com>
2025-11-06 23:27:14 +0300

    Update edit.php

--- views/customers/edit.php
+++ views/customers/edit.php
@@ -1,13 +1,23 @@
 <?php ob_start(); $cfg = require __DIR__ . '/../../config/config.php'; ?>
 <h1>تعديل عميل</h1>
+
 <form method="post" action="<?= $cfg['APP_URL'] ?>/customers/edit?id=<?= $customer['id'] ?>">
   <input type="hidden" name="csrf" value="<?= Utils::csrfToken() ?>">
-  <label>الاسم</label><input class="input" name="name" value="<?= Utils::h($customer['name']) ?>" required>
-  <label>الهاتف</label><input class="input" name="phone" value="<?= Utils::h($customer['phone']) ?>" required>
-  <label>المنطقة</label><input class="input" name="area" value="<?= Utils::h($customer['area']) ?>">
+  <input type="hidden" name="id" value="<?= $customer['id'] ?>">
+
+  <label>الاسم</label>
+  <input class="input" name="name" value="<?= Utils::h($customer['name']) ?>" required>
+
+  <label>الهاتف</label>
+  <input class="input" name="phone" value="<?= Utils::h($customer['phone']) ?>" required>
+
+  <label>المنطقة</label>
+  <input class="input" name="area" value="<?= Utils::h($customer['area']) ?>">
+
   <div style="display:flex; gap:8px">
     <button class="btn" type="submit">تحديث</button>
     <a class="btn outline" href="<?= $cfg['APP_URL'] ?>/customers">رجوع</a>
   </div>
 </form>
+
 <?php $content = ob_get_clean(); include __DIR__ . '/../layout.php'; ?>
